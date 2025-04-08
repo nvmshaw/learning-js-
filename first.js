283297eb9d0c59a7ea5975c6f49555cd879fe3ad
nvmshaw <tejas.mark6@gmail.com>
2025-04-09 00:03:09 +0530

    const updat  Changes to be committed: 	new file:   first.js

---
+++ first.js
@@ -0,0 +1,12 @@
+const name = {
+    title : instagramProfile,
+    description : "Instagram profile of the user",
+    isFollow : true,
+    followers : 1000,
+    following : 500,
+    posts : 100,
+    isPrivate : false, 
+    isVerefied : true,
+    occupation : entrepeneur,
+
+}
